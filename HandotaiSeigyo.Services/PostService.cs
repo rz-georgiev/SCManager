15b6f31ae61675eca7eecd33a9aeafd0e27d89a1
RGeorgiev <rzgeorgiev@outlook.com>
2020-05-05 15:51:22 +0300

    Added main page posts

---
+++ HandotaiSeigyo.Services/PostService.cs
@@ -0,0 +1,24 @@
+﻿using HandotaiSeigyo.Data;
+using HandotaiSeigyo.Data.Interfaces;
+using HandotaiSeigyo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HandotaiSeigyo.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly HandotaiDbContext _context;
+
+        public PostService(HandotaiDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Post> GetLastDayPosts()
+        {
+            return _context.Posts
+                .Where(x => x.CreatedDateTime >= DateTime.Today);
+        }
+    }
+}
---
+++ HandotaiSeigyo/ViewModels/Home/PostViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandotaiSeigyo.ViewModels.Home
+{
+    public class PostViewModel
+    {
+        public IEnumerable<PostListingViewModel> Posts { get; set; }
+    }
+}
