b8951b889bf58e440b26376ad6ea912b2f032024
RGeorgiev <rzgeorgiev@outlook.com>
2020-05-19 19:39:33 +0300

    Login page refactored

--- SCManager/Controllers/HomeController.cs
+++ SCManager/Controllers/HomeController.cs
@@ -1,54 +1,21 @@
-﻿using SCManager.Data.Interfaces;
-using SCManager.Data.Models;
-using SCManager.ViewModels;
-using SCManager.ViewModels.Home;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using SCManager.Data.Interfaces;
+using SCManager.ViewModels;
 using System.Diagnostics;
-using System.Linq;
 
 namespace SCManager.Controllers
 {
-    [AllowAnonymous]
+    [Authorize]
     public class HomeController : Controller
     {
-        private readonly IPostService _postsService;
-
-        public HomeController(IPostService postService)
+        public HomeController()
         {
-            _postsService = postService;
         }
 
         public IActionResult Index()
         {
-            var posts = _postsService.GetLast15Posts();
-            var first = posts.Take(5);
-            var second = posts.Skip(5).Take(5);
-            var third = posts.Skip(10).Take(5);
-
-            var result = new PostViewModel();
-            result.PostLists = new List<List<PostListingViewModel>>
-            {
-                GetPostListingViewModels(first),
-                GetPostListingViewModels(second),
-                GetPostListingViewModels(third),
-            };
-
-            return View(result);
-        }
-
-        private List<PostListingViewModel> GetPostListingViewModels(IEnumerable<Post> posts)
-        {
-            var models = posts.Select(x => new PostListingViewModel
-            {
-                Name = x.Name,
-                Description = x.Description,
-                ImagePath = x.ImagePath,
-                DateTime = x.CreatedDateTime.ToString("dd.MM.yyyy HH:mm")
-            }).ToList();
-
-            return models;
+            return View();
         }
 
         public IActionResult Privacy()
@@ -60,20 +27,6 @@ namespace SCManager.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        private IEnumerable<PostListingViewModel> GetPostViewModels(IEnumerable<Post> posts)
-        {
-            var newPosts = posts.OrderByDescending(x => x.Id)
-               .Select(x => new PostListingViewModel
-               {
-                   Name = x.Name,
-                   Description = x.Description,
-                   ImagePath = x.ImagePath,
-                   DateTime = x.CreatedDateTime.ToString("dd.MM.yyyy HH:mm")
-               });
-
-            return newPosts;
         }
     }
 }
