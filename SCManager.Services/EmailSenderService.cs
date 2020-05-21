e07e2ca0bc1878ad4e2f1b3b2cbf790ea4393f93
RGeorgiev <rzgeorgiev@outlook.com>
2020-05-21 13:09:39 +0300

    Added confirmation mails and ports

--- SCManager.Services/EmailSenderService.cs
+++ SCManager.Services/EmailSenderService.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using SCManager.Data;
+using System;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -20,16 +22,19 @@ namespace SCManager.Services
             var username = _configuration["MailSenderCredentials:Username"];
             var key = _configuration["MailSenderCredentials:Key"];
             var host = _configuration["MailSenderCredentials:Host"];
+            var port = _configuration["MailSenderCredentials:Port"];
 
             using SmtpClient smtpClient = new SmtpClient();
             var basicCredential = new NetworkCredential(username, key);
-         
+
             using MailMessage message = new MailMessage();
             MailAddress fromAddress = new MailAddress(username);
 
             smtpClient.Host = host;
+            smtpClient.Port = Convert.ToInt32(port);
             smtpClient.UseDefaultCredentials = false;
             smtpClient.Credentials = basicCredential;
+            smtpClient.EnableSsl = false;
 
             message.From = fromAddress;
             message.Subject = "Confirmation link";
@@ -39,7 +44,14 @@ namespace SCManager.Services
             message.Body = messageContent;
 
             message.To.Add(receiverEmail);
-            await smtpClient.SendMailAsync(message);
+            try
+            {
+                await smtpClient.SendMailAsync(message);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            };
         }
     }
 }
