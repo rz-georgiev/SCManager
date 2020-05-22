9657d05dfb5310ec414fce6a873dff3c728afca4
RGeorgiev <rzgeorgiev@outlook.com>
2020-05-22 22:24:47 +0300

    2FA activated + integrated QR Code

--- SCManager/Controllers/HomeController.cs
+++ SCManager/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SCManager.ViewModels;
 using System.Diagnostics;
--- SCManager/Startup.cs
+++ SCManager/Startup.cs
@@ -117,8 +117,13 @@ namespace SCManager
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "MyArea",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapRazorPages();
             });
         }
