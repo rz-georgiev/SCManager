122edb2682ba0c7fb7c60188c8cdcfc8444afef7
RGeorgiev <rzgeorgiev@outlook.com>
2020-05-25 21:59:01 +0300

    About text also gotten from db

--- SCManager/Controllers/AboutController.cs
+++ SCManager/Controllers/AboutController.cs
@@ -1,18 +1,30 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Ganss.XSS;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SCManager.Data.Interfaces;
+using SCManager.ViewModels.About;
+using System.Threading.Tasks;
 
 namespace SCManager.Controllers
 {
     [Authorize]
     public class AboutController : Controller
     {
-        public AboutController()
+        private readonly IStaticSiteInfoService _staticSiteInfoService;
+        private readonly HtmlSanitizer _htmlSanitizer;
+
+        public AboutController(IStaticSiteInfoService staticSiteInfoService, HtmlSanitizer htmlSanitizer)
         {
+            _staticSiteInfoService = staticSiteInfoService;
+            _htmlSanitizer = htmlSanitizer;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var info = await _staticSiteInfoService.GetByNameAsync("About");
+            var model = new IndexViewModel { Content = info.Content };
+
+            return View(model);
         }
     }
 }
