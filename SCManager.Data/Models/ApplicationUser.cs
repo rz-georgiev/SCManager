ca38ec8e8aef776fbc2fd963d98febee14114801
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-01 19:08:04 +0300

    Ignoring the cache files

--- SCManager.Data/Models/ApplicationUser.cs
+++ SCManager.Data/Models/ApplicationUser.cs
@@ -7,17 +7,13 @@
     {
         public string ImageId { get; set; }
 
-        public DateTime CreatedDateTime { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
         public DateTime? LastUpdatedDateTime { get; set; }
 
         public string LastUpdatedByUserId { get; set; }
 
-        public bool IsActive { get; set; }
-
-        public bool IsBanned { get; set; }
-
-        public ApplicationUser CreatedByUser { get; set; }
+        public bool IsBanned { get; set; } = false;
 
         public ApplicationUser LastUpdatedByUser { get; set; }
     }
