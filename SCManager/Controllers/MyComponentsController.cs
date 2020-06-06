37b18975ab159842d0bf0f3149ab6660da9f5031
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-06 20:45:28 +0300

    Prepared MyComponents and Dashboard[Home] views and controllers + models

---
+++ SCManager/Controllers/MyComponentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SCManager.Data.Interfaces;
+using System.Threading.Tasks;
+
+namespace SCManager.Controllers
+{
+    [Authorize]
+    public class MyComponentsController : Controller
+    {
+        private readonly IStaticSiteInfoService _staticSiteInfoService;
+
+        public MyComponentsController()
+        {
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
