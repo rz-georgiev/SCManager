4ac29c2fa6c2a9d32d62da62fa1933a77d0e35cd
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-06 22:27:08 +0300

    Dashboard and MyComponents prototype view assembled

--- SCManager/wwwroot/js/demo/datatables-demo.js
+++ SCManager/wwwroot/js/demo/datatables-demo.js
@@ -3,4 +3,6 @@ $(document).ready(function () {
     $('#dataTable_Users').DataTable({ "order": [[0, "desc"]] });
     $('#dataTable_ComponentTypes').DataTable({ "order": [[1, "desc"]] });
     $('#dataTable_UnitMultipliers').DataTable({ "order": [[0, "desc"]] });
+    $('#dataTable_MyComponents').DataTable({ "order": [[0, "desc"]] });
+
 });
