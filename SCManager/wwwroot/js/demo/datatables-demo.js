236a2568b70945e5ba7e02e88964d2b4e61c31f2
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-06 20:33:35 +0300

    Added unit multipliers to the admin panel view

--- SCManager/wwwroot/js/demo/datatables-demo.js
+++ SCManager/wwwroot/js/demo/datatables-demo.js
@@ -2,4 +2,5 @@
 $(document).ready(function () {
     $('#dataTable_Users').DataTable({ "order": [[0, "desc"]] });
     $('#dataTable_ComponentTypes').DataTable({ "order": [[1, "desc"]] });
+    $('#dataTable_UnitMultipliers').DataTable({ "order": [[0, "desc"]] });
 });
