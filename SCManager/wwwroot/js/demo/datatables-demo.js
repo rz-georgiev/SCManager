a00f02ca09362dbefedb4e5b05dfb48b7ba3e14c
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-06 20:28:48 +0300

    Component types registered as view

--- SCManager/wwwroot/js/demo/datatables-demo.js
+++ SCManager/wwwroot/js/demo/datatables-demo.js
@@ -1,12 +1,5 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-    $('#dataTable_Users').DataTable({
-        "order": [[2, "desc"]] 
-    });
-    $('#dataTable_ComponentTypes').DataTable({
-        "order": [[0, "desc"]]
-    });
-    $('#dataTable_UnitMultipliers').DataTable({
-        "order": [[0, "desc"]]
-    });
+$(document).ready(function () {
+    $('#dataTable_Users').DataTable({ "order": [[0, "desc"]] });
+    $('#dataTable_ComponentTypes').DataTable({ "order": [[1, "desc"]] });
 });
