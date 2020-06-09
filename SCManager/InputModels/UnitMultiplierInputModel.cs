732ac1fe8e577d5c08f38db8c4113d3bc7f47db2
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-09 20:30:44 +0300

    Added unit multipliers settings

---
+++ SCManager/InputModels/UnitMultiplierInputModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace SCManager.InputModels
+{
+    public class UnitMultiplierInputModel
+    {
+        public int? Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
