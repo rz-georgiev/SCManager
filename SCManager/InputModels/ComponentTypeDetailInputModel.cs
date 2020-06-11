7852ab2ffb81c88834405df931ae36973623e2cb
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-11 16:13:55 +0300

    Checking ModelDetails for null -temp minor fix

--- SCManager/InputModels/ComponentTypeDetailInputModel.cs
+++ SCManager/InputModels/ComponentTypeDetailInputModel.cs
@@ -1,13 +1,18 @@
-﻿namespace SCManager.InputModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SCManager.InputModels
 {
     public class ComponentTypeDetailInputModel
     {
         public int? Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Unit { get; set; }
 
+        [Required]
         public string Symbol { get; set; }
     }
 }
