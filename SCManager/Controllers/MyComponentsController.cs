079c16cfff1548a5dfec180389dbef3229279d2e
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-12 13:42:17 +0300

    Part 1 of MyComponents logic integration #21

--- SCManager/Controllers/MyComponentsController.cs
+++ SCManager/Controllers/MyComponentsController.cs
@@ -1,18 +1,65 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SCManager.Data.Interfaces;
+using SCManager.Data.Models;
+using SCManager.ViewModels.MyComponents;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SCManager.Controllers
 {
     [Authorize]
     public class MyComponentsController : Controller
     {
-        public MyComponentsController()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IMapper _mapper;
+        private readonly IComponentTypeService _componentTypeService;
+        private readonly IUnitMultiplierService _unitMultiplierService;
+        private readonly IStaticSiteInfoService _staticSiteInfoService;
+        private readonly IUserComponentTypeService _userComponentTypeService;
+
+        public MyComponentsController
+        (
+            UserManager<ApplicationUser> userManager,
+            IMapper mapper,
+            IComponentTypeService componentTypeService,
+            IUnitMultiplierService unitMultiplierService,
+            IStaticSiteInfoService staticSiteInfoService,
+            IUserComponentTypeService userComponentTypeService
+        )
         {
+            _userManager = userManager;
+            _mapper = mapper;
+            _componentTypeService = componentTypeService;
+            _unitMultiplierService = unitMultiplierService;
+            _staticSiteInfoService = staticSiteInfoService;
+            _userComponentTypeService = userComponentTypeService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var userId = _userManager.GetUserId(User);
+            var userComponents = _userComponentTypeService.GetAllForUserId(userId);
+
+            var componentModels = new List<ComponentViewModel>();
+
+            componentModels.Add(new ComponentViewModel
+            {
+                Id = 1,
+                Name = "Capacitor",
+                Quantity = 5,
+                TotalPrice = 250,
+                Value = "10 pf"
+            });
+
+            var model = new IndexViewModel
+            {
+                Components = componentModels
+            };
+
+            return View(model);
         }
     }
 }
---
+++ SCManager/ViewModels/MyComponents/ComponentViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SCManager.ViewModels.MyComponents
+{
+    public class ComponentViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Value { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
