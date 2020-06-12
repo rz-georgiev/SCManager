0e93f06e3984123c4cc6c2eb28612b57b676159a
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-12 13:42:17 +0300

    Part 1 of MyComponents logic integration #21

--- SCManager/ViewModels/MyComponents/IndexViewModel.cs
+++ SCManager/ViewModels/MyComponents/IndexViewModel.cs
@@ -1,6 +1,9 @@
-﻿namespace SCManager.ViewModels.MyComponents
+﻿using System.Collections.Generic;
+
+namespace SCManager.ViewModels.MyComponents
 {
     public class IndexViewModel
     {
+        public IEnumerable<ComponentViewModel> Components { get; set; }
     }
 }
