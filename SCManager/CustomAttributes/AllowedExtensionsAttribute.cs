a7f16b1b6e9cb49484ca71197074114790eb33dd
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-15 16:28:03 +0300

    #24 Validated image files [size and extension]
    + created customValidateAttribute classes

---
+++ SCManager/CustomAttributes/AllowedExtensionsAttribute.cs
@@ -0,0 +1,33 @@
+﻿using AngleSharp.Text;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace SCManager.CustomAttributes
+{
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file == null)
+            {
+                return new ValidationResult("Please choose a file");
+            }
+            var extension = Path.GetExtension(file.FileName).ToLower();
+            if (!_extensions.Contains(extension))
+            {
+                return new ValidationResult($"Use the following formats: {string.Join(", ", _extensions)}");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
