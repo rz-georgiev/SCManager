b18d4af9bf797514c2808b1f6d0cac2adca7ba1c
RGeorgiev <rzgeorgiev@outlook.com>
2020-06-28 16:51:02 +0300

    Bayesian analysis
    Installed Infer.NET + added views and controllers
    TODO Integrate

---
+++ SCManager/Controllers/BayesianAnalysisController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML.Probabilistic.Models;
+using SCManager.ViewModels.BayesianAnalysis;
+using System.Threading.Tasks;
+
+namespace SCManager.Controllers
+{
+    public class BayesianAnalysisController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            string value = null;
+            await Task.Run(() =>
+            {
+                Variable<bool> firstCoin = Variable.Bernoulli(0.5);
+                Variable<bool> secondCoin = Variable.Bernoulli(0.5);
+
+                Variable<bool> bothHeads = firstCoin & secondCoin;
+                InferenceEngine engine = new InferenceEngine();
+
+                value = engine.Infer(bothHeads).ToString();
+            });
+            var model = new IndexViewModel
+            {
+                SomeValue = value
+            };
+            return View(model);
+        }
+    }
+}
