71e87f6a2dec184131c00e2b0c37a281acbcbd95
RGeorgiev <rzgeorgiev@outlook.com>
2020-07-03 14:56:31 +0300

    Temp check

---
+++ SCManager/Controllers/_GlobalSearchPartialController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SCManager.Controllers
+{
+    public class _GlobalSearchPartialController : Controller
+    {
+        public IActionResult _GlobalSearchPartial()
+        {
+            return View();
+        }
+    }
+}
