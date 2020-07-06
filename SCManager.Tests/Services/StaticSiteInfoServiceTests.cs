f9a93d91ee0432c25a62e5489c244a4549d6a23a
RGeorgiev <rzgeorgiev@outlook.com>
2020-07-06 19:57:43 +0300

    Implemented staticSiteInfos tests

--- SCManager.Tests/Services/StaticSiteInfoServiceTests.cs
+++ SCManager.Tests/Services/StaticSiteInfoServiceTests.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.Primitives;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SCManager.Data.Interfaces;
+using SCManager.Data.Models;
 using SCManager.Services;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace SCManager.Tests.Services
@@ -14,6 +15,54 @@ namespace SCManager.Tests.Services
         public StaticSiteInfoServiceTests()
         {
             _staticSiteInfoService = new StaticSiteInfoService(SCManagerDbContext);
+        }
+
+        [TestMethod]
+        public void GetAll_WhenCalled_ShouldntReturnNull()
+        {
+            var result = _staticSiteInfoService.GetAll();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetByIdAsync_WhenIdIsNull_ShouldReturnNull()
+        {
+            var result = await _staticSiteInfoService.GetByIdAsync(null);
+            Assert.IsNull(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(-2)]
+        public async Task GetByIdAsync_WhenIdIsInvalid_ShouldReturnNull(int value)
+        {
+            var result = await _staticSiteInfoService.GetByIdAsync(value);
+            Assert.IsNull(result);
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow(null)]
+        public async Task GetByNameAsync_WhenNameIsNullOrWhitespace_ShouldReturnNull(string value)
+        {
+            var result = await _staticSiteInfoService.GetByNameAsync(value);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public async Task SaveChangesAsync_WhenARgumentIsNull_ShouldReturnFalse()
+        {
+            var result = await _staticSiteInfoService.SaveChangesAsync(null);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task SaveChangesAsync_WhenARgumentIdIsInvalid_ShouldReturnFalse()
+        {
+            var info = new StaticSiteInfo();
+
+            var result = await _staticSiteInfoService.SaveChangesAsync(info);
+            Assert.IsFalse(result);
         }
     }
 }
