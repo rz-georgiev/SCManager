42a6e2b467c01c9ce731674ebd59e3c68145b902
RGeorgiev <rzgeorgiev@outlook.com>
2020-08-05 23:16:26 +0300

    Minor bug fixes
    Started refactoring of the ConfigureServices method;
    Changed SendGrid credentials [added new account];
    TODO start working on the SSL certification;

--- SCManager/Startup.cs
+++ SCManager/Startup.cs
@@ -22,6 +22,8 @@ namespace SCManager
 {
     public class Startup
     {
+        private IServiceCollection _services;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -32,100 +34,24 @@ namespace SCManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential
-                // cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                // requires using Microsoft.AspNetCore.Http;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            _services = services;
 
-            services.AddDbContext<SCManagerDbContext>
-            (
-                options => options
-                .UseSqlServer(Configuration.GetConnectionString("SCManagerDbConnection"))
-            );
-
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<SCManagerDbContext>();
-
-            // ---------- Cloudinary region ----------
-            // Register your services here
-            Account account = new Account(
-                      Configuration["Cloudinary:CloudName"],
-                      Configuration["Cloudinary:ApiKey"],
-                      Configuration["Cloudinary:ApiSecret"]);
-
-            var cloudinary = new Cloudinary(account);
-            // ---------- Cloudinary region ----------
-
-            services.AddSingleton(cloudinary);
-            services.AddSingleton(new HtmlSanitizer());
-            services.AddSingleton<ICloudinaryService, CloudinaryService>();
-            services.AddSingleton<ISendGridService, SendGridService>();
-
-            services.AddTransient<IUnitMultiplierService, UnitMultiplierService>();
-            services.AddTransient<IComponentTypeService, ComponentTypeService>();
-            services.AddTransient<IComponentTypeDetailService, ComponentTypeDetailService>();
-            services.AddTransient<IStaticSiteInfoService, StaticSiteInfoService>();
-            services.AddTransient<IUserComponentTypeService, UserComponentTypeService>();
-            services.AddTransient<IUserComponentTypeDetailService, UserComponentTypeDetailService>();
-            services.AddTransient<ISearchService, SearchService>();
+            ConfigureHttpSettings();
+            ConfigureCookiePolicyOptions();
+            ConfigureDbContext();
+            ConfigureIdentityRolesOptions();
+            ConfigureConcreteServices();
 
             services.AddResponseCompression(options => options.EnableForHttps = true);
             services.Configure<DataProtectionTokenProviderOptions>(o => o.TokenLifespan = TimeSpan.FromMinutes(5));
-
             services.AddAutoMapper(typeof(Startup));
-            services.AddControllers(config =>
-            {
-                // using Microsoft.AspNetCore.Mvc.Authorization;
-                // using Microsoft.AspNetCore.Authorization;
-                var policy = new AuthorizationPolicyBuilder()
-                                 .RequireAuthenticatedUser()
-                                 .Build();
-                config.Filters.Add(new AuthorizeFilter(policy));
-                config.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-            });
+
+            ConfigureControllers();
 
             services.AddRazorPages();
-            services.Configure<IdentityOptions>(options =>
-            {
-                // Password settings.
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequireUppercase = true;
-                options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
 
-                // Lockout settings.
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
-                options.Lockout.MaxFailedAccessAttempts = 5;
-                options.Lockout.AllowedForNewUsers = true;
-
-                // User settings.
-                options.User.AllowedUserNameCharacters =
-                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-                options.User.RequireUniqueEmail = false;
-            });
-
-            services.ConfigureApplicationCookie(options =>
-            {
-                // Cookie settings
-                options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
-
-                options.LoginPath = "/Identity/Account/Login";
-                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-                options.SlidingExpiration = true;
-            });
-
-            services.Configure<CookieTempDataProviderOptions>(options =>
-            {
-                options.Cookie.IsEssential = true;
-            });
+            ConfigureIdentityOptions();
+            ConfigureCookies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -143,6 +69,7 @@ namespace SCManager
                 app.UseExceptionHandler("/Error/Index");
                 app.UseHsts();
             }
+
             app.UseResponseCompression();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -162,6 +89,136 @@ namespace SCManager
                     pattern: "{controller=Home}/{action=Index}/{id?}");
 
                 endpoints.MapRazorPages();
+            });
+        }
+
+        // Separating the ConfigureServices method into different pieces
+        // Its too damn big otherwise.
+        private void ConfigureHttpSettings()
+        {
+            //_services.AddHttpsRedirection(options =>
+            //{
+            //    options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+            //    options.HttpsPort = 5001;
+            //});
+
+            //_services.AddHsts(options =>
+            //{
+            //    options.Preload = true;
+            //    options.IncludeSubDomains = true;
+            //    options.MaxAge = TimeSpan.FromDays(60);
+            //    //options.ExcludedHosts.Add("scmananager.eu");
+            //    //options.ExcludedHosts.Add("www.scmanager.eu");
+            //});
+        }
+
+        private void ConfigureCookiePolicyOptions()
+        {
+            _services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential
+                // cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                
+                // requires using Microsoft.AspNetCore.Http;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+               // options.Secure = CookieSecurePolicy.SameAsRequest;
+            });
+        }
+
+        private void ConfigureDbContext()
+        {
+            _services.AddDbContext<SCManagerDbContext>
+            (
+                options => options
+                .UseSqlServer(Configuration.GetConnectionString("SCManagerDbConnection"))
+            );
+        }
+
+        private void ConfigureIdentityRolesOptions()
+        {
+            _services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+               .AddRoles<IdentityRole>()
+               .AddEntityFrameworkStores<SCManagerDbContext>();
+        }
+
+        private void ConfigureConcreteServices()
+        {
+            Account account = new Account(
+                    Configuration["Cloudinary:CloudName"],
+                    Configuration["Cloudinary:ApiKey"],
+                    Configuration["Cloudinary:ApiSecret"]);
+
+            var cloudinary = new Cloudinary(account);
+
+            _services.AddSingleton(cloudinary);
+            _services.AddSingleton(new HtmlSanitizer());
+            _services.AddSingleton<ICloudinaryService, CloudinaryService>();
+            _services.AddSingleton<ISendGridService, SendGridService>();
+
+            _services.AddTransient<IUnitMultiplierService, UnitMultiplierService>();
+            _services.AddTransient<IComponentTypeService, ComponentTypeService>();
+            _services.AddTransient<IComponentTypeDetailService, ComponentTypeDetailService>();
+            _services.AddTransient<IStaticSiteInfoService, StaticSiteInfoService>();
+            _services.AddTransient<IUserComponentTypeService, UserComponentTypeService>();
+            _services.AddTransient<IUserComponentTypeDetailService, UserComponentTypeDetailService>();
+            _services.AddTransient<ISearchService, SearchService>();
+        }
+
+        private void ConfigureControllers()
+        {
+            _services.AddControllers(config =>
+            {
+                // using Microsoft.AspNetCore.Mvc.Authorization;
+                // using Microsoft.AspNetCore.Authorization;
+                var policy = new AuthorizationPolicyBuilder()
+                                 .RequireAuthenticatedUser()
+                                 .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+                config.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            });
+        }
+
+        private void ConfigureIdentityOptions()
+        {
+            _services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+        }
+
+        private void ConfigureCookies()
+        {
+            _services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+            _services.Configure<CookieTempDataProviderOptions>(options =>
+            {
+                options.Cookie.IsEssential = true;
             });
         }
     }
