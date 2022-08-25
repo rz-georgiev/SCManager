8f914d970919aca92e1aa284fe20f9fabfcb83a2
Rado. G <rzgeorgiev@outlook.com>
2022-08-25 14:45:57 +0300

    Updated data seeder, Changed Cloudinary keys, Changed sendgrid keys

--- SCManager.Data/Constants.cs
+++ SCManager.Data/Constants.cs
@@ -2,8 +2,8 @@
 {
     public  static class Constants
     {
-        public static string BaseImageUrl = "https://res.cloudinary.com/dffy4iztl/image/upload/";
+        public static string BaseImageUrl = "https://res.cloudinary.com/scmanager/image/upload/";
 
-        public static string DefaultImageId = $"profile_ksrlpl.jpg";
+        public static string DefaultImageId = $"rop1msfzeqeij4msyyc5.png";
     }
 }
